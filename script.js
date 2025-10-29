09b962c93884351fc40a27fe52fd0152cbf694fe
Judith Olivia <jxdith13@gmail.com>
2025-10-29 05:02:47 -0400

    corrected js code

--- script.js
+++ script.js
@@ -1,57 +1,57 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const grid = document.querySelector('.grid')
-    let square = Array.from(document.querySelectorAll('.grid div'))
-    const scoreDisplay = document.querySelector('#score')
-    const startBn = document.querySelector('#start-button')
-    const width = 10
-    let nextRandom = 0 
-    let timerId 
-    let score = 0
-    const colors = [
-        'orange',
-        'red',
-        'purple',
-        'green',
-        'blue'
-    ]
+  const grid = document.querySelector('.grid')
+  let squares = Array.from(document.querySelectorAll('.grid div'))
+  const scoreDisplay = document.querySelector('#score')
+  const startBtn = document.querySelector('#start-button')
+  const width = 10
+  let nextRandom = 0
+  let timerId
+  let score = 0
+  const colors = [
+    'orange',
+    'red',
+    'purple',
+    'green',
+    'blue'
+  ]
 
     // tetrominos
-    const ITetromino = [
-        [1, width+1, width*2 + 1, 2],
-        [width, width+1, width+2, width*2 + 2],
-        [1, width+1, width*2 + 1, 2],
-        [width, width+1, width+2, width*2 + 2]
+    const lTetromino = [
+        [1, width+1, width*2+1, width*2+2],
+        [width, width+1, width+2, width*2],
+        [0, 1, width+1, width*2+1],
+        [width+2, width*2, width*2+1, width*2+2]
     ]
 
     const zTetromino = [
-        [0, width, width+1, width*2 + 1],
-        [width+1, width+2, width*2, width*2 + 1],
-        [0, width, width+1, width*2 + 1],
-        [width+1, width+2, width*2, width*2 + 1]
+        [0, width, width+1, width*2+1],
+        [width+1, width+2, width*2, width*2+1],
+        [0, width, width+1, width*2+1],
+        [width+1, width+2, width*2, width*2+1]
     ]
 
-    const tTetramino = [
+    const tTetromino = [
         [1, width, width+1, width+2],
-        [1, width+1, width+2,width*2 + 1],
-        [width,width+1,width+2,width*2+1],
-        [1,width,width+1,width*2+1]
+        [1, width+1, width+2, width*2+1],
+        [width, width+1, width+2, width*2+1],
+        [1, width, width+1, width*2+1]
     ]
 
     const oTetromino = [
-        [0,1,width,width+1],
-        [0,1,width,width+1],
-        [0,1,width,width+1],
-        [0,1,width,width+1]
+        [0, 1, width, width+1],
+        [0, 1, width, width+1],
+        [0, 1, width, width+1],
+        [0, 1, width, width+1]
     ]
 
     const iTetromino = [
-        [1, width+1, width*2 + 1, width*3 + 1],
+        [1, width+1, width*2+1, width*3+1],
         [width, width+1, width+2, width+3],
-        [1, width+1, width*2 + 1, width*3 +1],
+        [1, width+1, width*2+1, width*3+1],
         [width, width+1, width+2, width+3]
     ]
 
-    const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
+    const theTetrominoes = [ITetromino, zTetromino, tTetromino, oTetromino, iTetromino]
 
     let currentPosition = 4
     let currentRotation = 0
@@ -78,17 +78,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function moveDown() {
         undraw()
-        cureentPosition += width 
+        currentPosition += width
         draw()
         freeze()
     }
 
     function freeze() {
-        if(current.some(index => squared[currentPosition + index + width].classList.constains('taken'))) {
+        if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
             current.forEach(index => squares[currentPosition + index].classList.add('taken'))
 
             random = nextRandom
-            nextRandom = Math.floor(Math.random() * theTertrominoes.length)
+            nextRandom = Math.floor(Math.random() * theTetrominoes.length)
             current = theTetrominoes[random][currentRotation]
             currentPosition = 4
 
@@ -122,13 +122,13 @@ document.addEventListener('DOMContentLoaded', () => {
         if ((P+1) % width < 4) { 
             if (isAtRight()){
                 currentPosition += 1 
-                checkRotatedPosition(P) 
+                checkRotatedPosition(P)
             }
         }
         else if (P % width > 5) {
             if (isAtLeft()){
                 currentPosition -= 1
-            checkRotatePosition(P)
+                checkRotatedPosition(P)
             }
         }
     }
@@ -137,7 +137,7 @@ document.addEventListener('DOMContentLoaded', () => {
         undraw()
         currentRotation ++
         if(currentRotation === current.length) {
-            currentRosition = 0
+            currentRotation = 0
         }
         current = theTetrominoes[random][currentRotation]
         checkRotatedPosition()
@@ -149,11 +149,11 @@ document.addEventListener('DOMContentLoaded', () => {
     const displayIndex = 0
 
     const upNextTetrominoes = [
-        [1, displayWidth+1, displayWidth*2+1, 2], 
-        [0, displayWidth, displayWidth+1, displayWidth*2+1], 
-        [1, displayWidth, displayWidth+1, displayWidth+2], 
-        [0, 1, displayWidth, displayWidth+1], 
-        [1, displayWidth+1, displayWidth*2+1, displayWidth*3+1] 
+        [1, displayWidth+1, displayWidth*2+1, displayWidth*2+2],  // L shape
+        [0, displayWidth, displayWidth+1, displayWidth*2+1],      // Z shape  
+        [1, displayWidth, displayWidth+1, displayWidth+2],        // T shape
+        [0, 1, displayWidth, displayWidth+1],                     // O shape
+        [1, displayWidth+1, displayWidth*2+1, displayWidth*3+1]   // I shape
     ]
 
     function displayShape(){
