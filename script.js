dbd7b1f0999ec6be167b897f47cbbd7de24f1761
Judith Olivia <jxdith13@gmail.com>
2025-10-29 02:44:48 -0400

    css

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-document.addEventListener
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid')
+    let square = Array.from(document.querySelectorAll('.grid div'))
+    const scoreDisplay = document.querySelector('#score')
+    const startBn = document.querySelector('#start-button')
+    const width = 10
+    let nextRandom = 0 
+    let timerId 
+    let score = 0
+    const colors = [
+        'orange',
+        'red',
+        'purple',
+        'green',
+        'blue'
+    ]
+})
