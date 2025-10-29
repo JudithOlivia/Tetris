186130ff0fb176a0844a1d189c0d9b5953b7e460
Judith Olivia <jxdith13@gmail.com>
2025-10-29 04:47:36 -0400

    gameover

--- script.js
+++ script.js
@@ -180,6 +180,31 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     })
 
+    function addScore() {
+        for (let i = 0; i < 199; i +=width) {
+        const row = [i, i+1, i+2, i+3, i+4, i+5, i+6, i+7, i+8, i+9]
+
+        if(row.every(index => squares[index].classList.contains('taken'))) {
+            score +=10
+            scoreDisplay.innerHTML = score
+            row.forEach(index => {
+            squares[index].classList.remove('taken')
+            squares[index].classList.remove('tetromino')
+            squares[index].style.backgroundColor = ''
+            })
+            const squaresRemoved = squares.splice(i, width)
+            squares = squaresRemoved.concat(squares)
+            squares.forEach(cell => grid.appendChild(cell))
+        }
+        }
+    }
+
+    function gameOver() {
+        if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+        scoreDisplay.innerHTML = 'end'
+        clearInterval(timerId)
+        }
+    }
     
 
 })
