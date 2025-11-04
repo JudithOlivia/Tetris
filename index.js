41154bef94b7dad0fc09e007595d997da67e075a
Judith Olivia <jxdith13@gmail.com>
2025-11-04 10:37:15 -0500

    add score

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const width = 10
     let nextRandom = 0
     let timerId 
+    let score = 0
 
     // Tetrominoes 
     const lTetromino = [
@@ -131,7 +132,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let displayIndex = 0
     let nextRandom = 0
 
-    const const upNextTetrominoes = [
+    const upNextTetrominoes = [
         [1, displayWidth+1, displayWidth*2+1, 2], //lTetromino
         [0, displayWidth, displayWidth+1, displayWidth*2+1], //zTetromino
         [1, displayWidth, displayWidth+1, displayWidth+2], //tTetromino
@@ -157,9 +158,28 @@ document.addEventListener('DOMContentLoaded', () => {
         } else {
             draw()
             timerId = setInterval(moveDown, 1000)
-            nextRandom = Math.floor(Math.random[]*theTetrominoes.length)
+            nextRandom = Math.floor(Math.random()*theTetrominoes.length)
             displayShape()
         }
     })
+
+    function addScore() {
+        for (let i = 0; i < 199; i +=width) {
+            const row = [i, i+1, i+2, i+3, i+4, i+5, i+6, i+7, i+8, i+9]
+
+            if(row.every(index => squares[index].classList.contains('taken'))) {
+                score +=10
+                scoreDisplay.innerHTML = score
+                row.forEach(index => {
+                    squares[index].classList.remove('taken')
+                    squares[index].classList.remove('tetromino')
+                    squares[index].style.backgroundColor = ''
+                })
+                const squaresRemoved = squares.splice(i, width)
+                squares = squaresRemoved.concat(squares)
+                squares.forEach(cell => grid.appendChild(cell))
+            }
+        }
+    }
 
 })
